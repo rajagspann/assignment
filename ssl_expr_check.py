006dce4757d0abaa68f68e235c4401249d675684
rajagspann <rajak.dop@gmail.com>
2021-04-13 12:33:56 +0000

    updated

--- ssl_expr_check.py
+++ ssl_expr_check.py
@@ -6,31 +6,41 @@ import socket
 import ssl
 import datetime
 
+#dictionary
+d = {}
 
-class ssl_check():
-
-
-    def __init__(self, hostname):
-        print "%s"%(hostname)
-        ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
-        context = ssl.create_default_context()
-        # to wrap a socket.
-        conn = context.wrap_socket(socket.socket(socket.AF_INET),server_hostname=hostname,)
-        conn.settimeout(3.0)
-        conn.connect((hostname, 443))
-        ssl_info = conn.getpeercert()
-        Exp_ON=datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
-        Days_Remaining= Exp_ON - datetime.datetime.utcnow()
-        print "Expires ON:- %s\nRemaining:- %s" %(Exp_ON,Days_Remaining)
-        print "-----------------------------------------------"
-
-
-
-domains = ['apple.com', 'google.com', 'facebook.com', 'netflix.com', 'yahoo.com']
-
-
-# I am using map function to iterate through the list
+print "-----Certificate details without sort-----"
+print "=========================================="
+def scheck(hostname):
+    print "%s"%(hostname)
+    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+    context = ssl.create_default_context()
+    # to wrap a socket.
+    conn = context.wrap_socket(socket.socket(socket.AF_INET),server_hostname=hostname,)
+    conn.settimeout(3.0)
+    conn.connect((hostname, 443))
+    ssl_info = conn.getpeercert()
+    Exp_ON=datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+    d[hostname] = Exp_ON
+    Days_Remaining= Exp_ON - datetime.datetime.utcnow()
+   #d[hostname] = Days_Remaining
+    print "Expires ON:- %s\nRemaining:- %s" %(Exp_ON,Days_Remaining)
+    print "-----------------------------------------------"
 
 
-map(ssl_check, domains)
+
+with open('websites') as f:
+    a = f.readlines()
+
+lines = list(line for line in (l.strip() for l in a) if line)
+map(scheck, lines)
+
+print('\n')
+print('\n')
+
+print "-----Certificate details with sort-----"
+print "=========================================="
+for k in sorted(d, key=d.get, reverse=True):
+    print(k, d[k])
+
 
