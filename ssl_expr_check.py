3ea27feb35b85d47d82d05b865c13f1ada5c1c0a
rajagspann <rajak.dop@gmail.com>
2021-04-13 15:05:01 +0000

    update file

--- ssl_expr_check.py
+++ ssl_expr_check.py
@@ -9,10 +9,10 @@ import datetime
 #dictionary
 d = {}
 
-print "-----Certificate details without sort-----"
-print "=========================================="
+#print "-----Certificate details without sort-----"
+#print "=========================================="
 def scheck(hostname):
-    print "%s"%(hostname)
+  #  print "%s"%(hostname)
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
     context = ssl.create_default_context()
     # to wrap a socket.
@@ -24,8 +24,8 @@ def scheck(hostname):
     d[hostname] = Exp_ON
     Days_Remaining= Exp_ON - datetime.datetime.utcnow()
    #d[hostname] = Days_Remaining
-    print "Expires ON:- %s\nRemaining:- %s" %(Exp_ON,Days_Remaining)
-    print "-----------------------------------------------"
+   # print "Expires ON:- %s\nRemaining:- %s" %(Exp_ON,Days_Remaining)
+   # print "-----------------------------------------------"
 
 
 
@@ -40,7 +40,10 @@ print('\n')
 
 print "-----Certificate details with sort-----"
 print "=========================================="
+
+#for k in sorted(d.items(), key = lambda x:datetime.datetime.strptime(x[0], '%d-%m-%Y'), reverse=True):
+
 for k in sorted(d, key=d.get, reverse=True):
-    print(k, d[k])
+    print(k, d[k].strftime('%d/%m/%Y'))
 
 
